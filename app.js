3a0206b8f3435765e60e955edaf42863ce646f66
ronchitech <ronchibusiness@gmail.com>
2022-07-14 22:10:53 +0800

    array types

--- app.js
+++ app.js
@@ -8,6 +8,15 @@
 //better syntax
 var person = {
     name: 'Ronchi',
-    age: 24
+    age: 24,
+    hobbies: ['sports', 'sleeping']
 };
+var favoriteActivities;
+favoriteActivities = ['playing', 'cooking'];
+var birthDate; //any[] type
+birthDate = ['April', 22, 1998];
 console.log(person.age);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -10,6 +10,18 @@
 const person = {
   name: 'Ronchi',
   age: 24,
+  hobbies: ['sports', 'sleeping'],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['playing', 'cooking'];
+
+// let birthDate: (string | number)[]; //any[] type
+// birthDate = ['April', 22, 1998];
+
 console.log(person.age);
+
+for(const hobby of person.hobbies){
+  console.log(hobby.toUpperCase());
+  
+}
