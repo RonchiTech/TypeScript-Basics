82cc4c9443e28c92cd24e7f7928636281689fd80
ronchitech <ronchibusiness@gmail.com>
2022-07-14 17:56:52 +0800

    typescript basics; using types

---
+++ app.js
@@ -0,0 +1,7 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+var number1 = '10';
+var number2 = 2.5;
+var result = add(Number(number1), Number(number2));
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,10 @@
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+const number1 = 10;
+const number2 = 2.5;
+
+const result = add(Number(number1), Number(number2));
+
+console.log(result);
