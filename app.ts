72fdedc05575d0944953de77044bebe266d0f09d
ronchitech <ronchibusiness@gmail.com>
2022-07-14 21:44:33 +0800

    type assignment and type interference

--- app.ts
+++ app.ts
@@ -8,9 +8,10 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   }
 }
 
-const number1 = 10;
+let number1: number;
+number1 = 25;
 const number2 = 2.5;
 const printResult = true;
-const phrase = 'The sum is: ';
-
+let phrase = 'The sum is: ';
+// phrase = 5
 add(Number(number1), Number(number2), printResult, phrase);
