9d89b110af7526d17d3ae55beec5b4ca0d1317fa
ronchitech <ronchibusiness@gmail.com>
2022-07-17 13:10:50 +0800

    basics, let const, arrow function, default func params, spread operator, rest params, array and object destructuring

--- app.js
+++ app.js
@@ -12,20 +12,36 @@
 //   console.log(a * b);
 // };
 // multiplyBy(2);
-const printRest = (...restParams) => {
-    console.log(restParams);
+//rest params
+// const printRest = (...restParams: string[] | number[]) => {
+//   console.log(restParams);
+// };
+// printRest('my', 'name', 'is', 'RFM');
+// const addRest = (...restParams: number[]) => {
+//   return restParams.reduce((prevValue, currValue) => {
+//     return prevValue + currValue;
+//   }, 0);
+// };
+// console.info(addRest(2, 5, 7, 15, 9.8));
+// const multiply = (...twoNumbers: [number, number]) => {
+//   return twoNumbers.reduce((prev, curr) => {
+//     return prev * curr;
+//   }, 1);
+// };
+// // multiply(2, 5, 8); //not allowed, expected only 2 arguments (based on the tuple)
+// multiply(2, 5);
+//array and object destructuring;
+//array
+const Hobbies = ['Eating', 'Dancing', 'Cooking'];
+const [hobby1, hobby2, hobby3] = Hobbies;
+const [firstHobby, ...restHobby] = Hobbies;
+console.log(hobby1, hobby2, hobby3);
+console.log(firstHobby, ...restHobby); //console.log(firstHobby, restHobby) ---> restHobby will be in array
+//object
+const person = {
+    name: 'Floyd',
+    age: 24,
+    gender: 'Male',
 };
-printRest('my', 'name', 'is', 'RFM');
-const addRest = (...restParams) => {
-    return restParams.reduce((prevValue, currValue) => {
-        return prevValue + currValue;
-    }, 0);
-};
-console.info(addRest(2, 5, 7, 15, 9.8));
-const multiply = (...twoNumbers) => {
-    return twoNumbers.reduce((prev, curr) => {
-        return prev * curr;
-    }, 1);
-};
-// multiply(2, 5, 8); //not allowed, expected only 2 arguments (based on the tuple)
-multiply(2, 5);
+const { name: myName, age, gender } = person; //myname is alias
+console.log(myName, age, gender);
