1179f20a7303b4a655df04cbaa198fe3e7398a3d
ronchitech <ronchibusiness@gmail.com>
2022-07-17 12:47:17 +0800

    rest params

--- app.js
+++ app.js
@@ -4,10 +4,15 @@
 //   console.log('Text is: ', message);
 // }
 // button.addEventListener('click', () => logger('hey'));
-// const logger: (m: string) => void = (message) => {
+// const logger2: (m: string) => void = (message) => {
 //   console.log(message);
 // };
+// logger2('testing');
 // const multiplyBy = (a: number, b: number = 2) => {
 //   console.log(a * b);
 // };
 // multiplyBy(2);
+const printRest = (...restParams) => {
+    console.log(restParams);
+};
+printRest('my', 'name', 'is', 'RFM');
--- app.ts
+++ app.ts
@@ -3,15 +3,21 @@
 // function logger(message: string) {
 //   console.log('Text is: ', message);
 // }
-
 // button.addEventListener('click', () => logger('hey'));
 
-// const logger: (m: string) => void = (message) => {
+// const logger2: (m: string) => void = (message) => {
 //   console.log(message);
 // };
+
+// logger2('testing');
 
 // const multiplyBy = (a: number, b: number = 2) => {
 //   console.log(a * b);
 // };
-
 // multiplyBy(2);
+
+const printRest = (...restParams: string[] | number[]) => {
+  console.log(restParams);
+};
+
+printRest('my', 'name', 'is', 'RFM');
