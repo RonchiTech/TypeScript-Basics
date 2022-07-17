59f976b3791c98ddcd138e3575eb6d4d92932d1f
ronchitech <ronchibusiness@gmail.com>
2022-07-17 12:36:46 +0800

    arrow function with a default parameter

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 "use strict";
-const button = document.querySelector('button');
-function logger(message) {
-    console.log('Text is: ', message);
-}
-button.addEventListener('click', () => logger('hey'));
+// const button = document.querySelector('button')! as HTMLButtonElement;
+// function logger(message: string) {
+//   console.log('Text is: ', message);
+// }
+// button.addEventListener('click', () => logger('hey'));
+// const logger: (m: string) => void = (message) => {
+//   console.log(message);
+// };
+// const multiplyBy = (a: number, b: number = 2) => {
+//   console.log(a * b);
+// };
+// multiplyBy(2);
--- app.ts
+++ app.ts
@@ -6,6 +6,12 @@
 
 // button.addEventListener('click', () => logger('hey'));
 
-const logger: (m: string) => void = (message) => {
-  console.log(message);
-};
+// const logger: (m: string) => void = (message) => {
+//   console.log(message);
+// };
+
+// const multiplyBy = (a: number, b: number = 2) => {
+//   console.log(a * b);
+// };
+
+// multiplyBy(2);
