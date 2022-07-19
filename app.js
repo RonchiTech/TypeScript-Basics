f09d827b9c66ce73290a40c528a3b9e56c5c17c5
ronchitech <ronchibusiness@gmail.com>
2022-07-19 10:38:10 +0800

    public and private access modifiers

--- app.js
+++ app.js
@@ -2,22 +2,34 @@
 class Department {
     constructor(n) {
         this.name = 'NO_DEPARTMENT_NAME';
-        this.description = '';
+        this.employees = [];
         this.name = n;
     }
-    addMembers() {
-        return;
-    }
-    addDescription(d) {
-        this.description = d;
-    }
     describe() {
-        console.log(`The ${this.name} Department  is for: ${this.description}`);
+        console.log(`This is the ${this.name}`);
+    }
+    add(employee) {
+        this.employees.push(employee);
+    }
+    returnEmployees() {
+        return this.employees;
+    }
+    showInfo() {
+        console.log(this.returnEmployees());
     }
 }
+// const accounting = new Department('Accounting');
+// accounting.describe();
+// const accountingCopy = { describe: accounting.describe };
+// // console.log(accounting);
+// const accDec1 = accountingCopy.describe.bind(accounting);
+// accDec1();
+// const accountingCopy2 = {
+//   name: 'TEST',
+//   describe: accounting.describe,
+// };
+// accountingCopy2.describe();
 const accounting = new Department('Accounting');
-accounting.addDescription("Accountants' department");
-accounting.describe();
-const accountingCopy = { describe: accounting.describe };
-// console.log(accounting);
-accountingCopy.describe.bind(accounting);
+accounting.add('Ronchi');
+// accounting.employees[1] = 'Floyd' //cannot do this once the property/field is set to private
+accounting.showInfo();
--- app.ts
+++ app.ts
@@ -1,27 +1,45 @@
 class Department {
   name: string = 'NO_DEPARTMENT_NAME';
-  description: string = '';
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
   }
-  addMembers() {
-    return;
-  }
-  addDescription(d: string) {
-    this.description = d;
-  }
+
   describe(this: Department) {
-    console.log(`The ${this.name} Department  is for: ${this.description}`);
+    console.log(`This is the ${this.name}`);
+  }
+
+  add(employee: string) {
+    this.employees.push(employee);
+  }
+  private returnEmployees() {
+    return this.employees;
+  }
+
+  showInfo(){
+    console.log(this.returnEmployees());
+    
   }
 }
 
+// const accounting = new Department('Accounting');
+// accounting.describe();
+
+// const accountingCopy = { describe: accounting.describe };
+// // console.log(accounting);
+// const accDec1 = accountingCopy.describe.bind(accounting);
+// accDec1();
+
+// const accountingCopy2 = {
+//   name: 'TEST',
+//   describe: accounting.describe,
+// };
+
+// accountingCopy2.describe();
+
 const accounting = new Department('Accounting');
-accounting.addDescription("Accountants' department");
-accounting.describe();
 
-const accountingCopy = { describe: accounting.describe };
-
-// console.log(accounting);
-accountingCopy.describe.bind(accounting);
-
+accounting.add('Ronchi');
+// accounting.employees[1] = 'Floyd' //cannot do this once the property/field is set to private
+accounting.showInfo()
