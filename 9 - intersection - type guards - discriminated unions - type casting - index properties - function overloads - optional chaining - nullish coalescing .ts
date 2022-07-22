f5eff5a1da1773a67d4addda8962b7271017daf3
ronchitech <ronchibusiness@gmail.com>
2022-07-22 20:45:23 +0800

    generics, built-in generics

---
+++ 9 - intersection - type guards - discriminated unions - type casting - index properties - function overloads - optional chaining - nullish coalescing .ts
@@ -0,0 +1,185 @@
+//1. Intersection Types
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type Promoted = Admin & Employee;
+
+let me: Promoted;
+
+me = {
+  name: 'Ronchi',
+  privileges: ['Update Server'],
+  startDate: new Date(),
+};
+
+console.log(me);
+
+type Result = string | number;
+type Numeric = number | boolean;
+
+type Intersected = Result & Numeric;
+
+//2. Type Guards
+function add(a: Result, b: Result) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    //this is the type guard
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+//2.1 Type Guards
+type UnknownEmployee = Admin | Employee;
+function printEmployeeInfo(employee: UnknownEmployee) {
+  console.log('Name: ', employee.name);
+  if ('privileges' in employee) {
+    //JS code that allows to check if 'privileges' exists as a property
+    console.log('Privileges: ', employee.privileges);
+  }
+  if ('startDate' in employee) {
+    //JS code that allows to check if 'startDate' exists as a property
+    console.log('startDate: ', employee.startDate);
+  }
+}
+
+printEmployeeInfo(me);
+
+//2.2 type guards on classes
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...');
+  }
+  loadCargo(amount: number) {
+    console.log('Loading cargo: ', amount);
+  }
+}
+
+type Vehicle = Truck | Car;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive(); //because this always exists!
+
+  // if ('loadCargo' in vehicle) {
+  //   vehicle.loadCargo(1500);
+  // }
+
+  //better way
+  if (vehicle instanceof Truck) {
+    //instanceof
+    vehicle.loadCargo(1500);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+//3. Discriminated Unions
+interface Bird {
+  //add a type
+  type: 'bird'; //literal type
+  flyingSpeed: number;
+}
+
+interface Horse {
+  //add a type
+  type: 'horse'; //literal type
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+      break;
+    default:
+      speed = 'Not animal';
+  }
+  console.log('Moving at speed ' + speed);
+}
+let viceGanda: Horse;
+viceGanda = { runningSpeed: 100, type: 'horse' };
+moveAnimal(viceGanda);
+
+//4 Type Casting
+// const button = document.querySelector('button');
+// const paragraph = document.getElementById('main-paragraph');
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!; // (!) tells TS it wont be null, type casting optional approach
+const userInputElement = document.getElementById(
+  'user-input'
+)! as HTMLInputElement;
+userInputElement.value = 'What is your name?';
+
+//if you are not sure if it exists then....
+const userInputElement2 = document.getElementById('user-input');
+if (userInputElement2) {
+  (userInputElement2 as HTMLInputElement).value = 'Hello World!';
+}
+
+//5. Index Properties
+interface ErrorContainer {
+  //{ email: 'Not a valid email', username: 'Must start with a letter }
+  [prop: string]: string;
+}
+
+const errorBags: ErrorContainer = {
+  email: 'Not a valid email',
+  password: 'Incorrect Password!',
+};
+
+//6 Function Overloads
+function combine(q: string, b: string): string;
+function combine(x: number, y: number): number;
+function combine(a: Result, b: Result) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    //this is the type guard
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+const interger = combine(3.33, 2.612);
+interger.toFixed(2);
+
+const letter = combine('Hello', 'World!');
+letter.split('');
+
+//7. Optional Chaining
+const myself = {
+  name: 'Ronchi',
+  age: 24,
+  job: {
+    title: 'Full Stack Developer',
+  },
+};
+console.log(myself.name);
+console.log(myself?.job?.title);
+
+const zero = 0;
+const knull = null;
+const myname = 'Ronchi';
+
+console.log(myname ?? 'No Name');
+console.log(knull ?? 'No Name');
+console.log(zero ?? 'No Name');
--- app.js
+++ app.js
@@ -1,117 +1,12 @@
 "use strict";
-var _a;
-let me;
-me = {
-    name: 'Ronchi',
-    privileges: ['Update Server'],
-    startDate: new Date(),
-};
-console.log(me);
-//2. Type Guards
-function add(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        //this is the type guard
-        return a.toString() + b.toString();
-    }
-    return a + b;
-}
-function printEmployeeInfo(employee) {
-    console.log('Name: ', employee.name);
-    if ('privileges' in employee) {
-        //JS code that allows to check if 'privileges' exists as a property
-        console.log('Privileges: ', employee.privileges);
-    }
-    if ('startDate' in employee) {
-        //JS code that allows to check if 'startDate' exists as a property
-        console.log('startDate: ', employee.startDate);
-    }
-}
-printEmployeeInfo(me);
-//2.2 type guards on classes
-class Car {
-    drive() {
-        console.log('Driving...');
-    }
-}
-class Truck {
-    drive() {
-        console.log('Driving a truck...');
-    }
-    loadCargo(amount) {
-        console.log('Loading cargo: ', amount);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicle(vehicle) {
-    vehicle.drive(); //because this always exists!
-    // if ('loadCargo' in vehicle) {
-    //   vehicle.loadCargo(1500);
-    // }
-    //better way
-    if (vehicle instanceof Truck) {
-        //instanceof
-        vehicle.loadCargo(1500);
-    }
-}
-useVehicle(v1);
-useVehicle(v2);
-function moveAnimal(animal) {
-    let speed;
-    switch (animal.type) {
-        case 'bird':
-            speed = animal.flyingSpeed;
-            break;
-        case 'horse':
-            speed = animal.runningSpeed;
-            break;
-        default:
-            speed = 'Not animal';
-    }
-    console.log('Moving at speed ' + speed);
-}
-let viceGanda;
-viceGanda = { runningSpeed: 100, type: 'horse' };
-moveAnimal(viceGanda);
-//4 Type Casting
-// const button = document.querySelector('button');
-// const paragraph = document.getElementById('main-paragraph');
-// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!; // (!) tells TS it wont be null, type casting optional approach
-const userInputElement = document.getElementById('user-input');
-userInputElement.value = 'What is your name?';
-//if you are not sure if it exists then....
-const userInputElement2 = document.getElementById('user-input');
-if (userInputElement2) {
-    userInputElement2.value = 'Hello World!';
-}
-const errorBags = {
-    email: 'Not a valid email',
-    password: 'Incorrect Password!',
-};
-function combine(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        //this is the type guard
-        return a.toString() + b.toString();
-    }
-    return a + b;
-}
-const interger = combine(3.33, 2.612);
-interger.toFixed(2);
-const letter = combine('Hello', 'World!');
-letter.split('');
-//7. Optional Chaining
-const myself = {
-    name: 'Ronchi',
-    age: 24,
-    job: {
-        title: 'Full Stack Developer',
-    },
-};
-console.log(myself.name);
-console.log((_a = myself === null || myself === void 0 ? void 0 : myself.job) === null || _a === void 0 ? void 0 : _a.title);
-const zero = 0;
-const knull = null;
-const myname = 'Ronchi';
-console.log(myname !== null && myname !== void 0 ? myname : 'No Name');
-console.log(knull !== null && knull !== void 0 ? knull : 'No Name');
-console.log(zero !== null && zero !== void 0 ? zero : 'No Name');
+//1. Generics
+const names = ['Ronchi'];
+// console.log(names[0].split(''));
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Hello World');
+    }, 3000);
+});
+promise.then((result) => {
+    console.log('Result: ', result);
+});
