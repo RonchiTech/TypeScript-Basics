e6825039401665352fed5b0dba574ecece462d43
ronchitech <ronchibusiness@gmail.com>
2022-07-22 21:06:44 +0800

    creating a generic function

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ const promise = new Promise((resolve, reject) => {
 promise.then((result) => {
     console.log('Result: ', result);
 });
+//2. Creating a Generic Function
+function merger(obj1, obj2) {
+    return Object.assign(Object.assign({}, obj1), obj2);
+}
+const mergedObj = merger({ name: 'Ronchi' }, { age: 24, male: true });
+mergedObj.age;
+// const mergedObj2 = merger< //just the same
+//   { name: string; hobbies: string[] },
+//   { age: number; male: boolean }
+// >({ name: 'Ronchi', hobbies: ['sleeping'] }, { age: 24, male: true });
--- app.ts
+++ app.ts
@@ -10,3 +10,15 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
 promise.then((result) => {
   console.log('Result: ', result);
 });
+
+//2. Creating a Generic Function
+function merger<T, U>(obj1: T, obj2: U) {
+  return { ...obj1, ...obj2 };
+}
+const mergedObj = merger({ name: 'Ronchi' }, { age: 24, male: true });
+mergedObj.age;
+
+// const mergedObj2 = merger< //just the same
+//   { name: string; hobbies: string[] },
+//   { age: number; male: boolean }
+// >({ name: 'Ronchi', hobbies: ['sleeping'] }, { age: 24, male: true });
