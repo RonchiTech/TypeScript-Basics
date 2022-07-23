3e92ac23dc3c9923551e64514c701e5304006b9e
ronchitech <ronchibusiness@gmail.com>
2022-07-23 19:53:50 +0800

    adding multiple decorators

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
 "use strict";
+// //1. Decorators
+// function Logger(constructor: Function) {
+//   //Decorator
+//   console.log('Logging...');
+//   console.log('constructor', constructor);
+// }
+// @Logger
+// class Person {
+//   name = 'Ronchi';
+//   constructor() {
+//     console.log('Creating Person Object....');
+//   }
+// }
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-//1. Decorators
-function Logger(constructor) {
-    //Decorator
-    console.log('Logging...');
-    console.log('constructor', constructor);
-}
-let Person = class Person {
-    constructor() {
-        this.name = 'Ronchi';
-        console.log('Creating Person Object....');
-    }
-};
-Person = __decorate([
-    Logger
-], Person);
-const person = new Person();
-console.log(person);
-//2. Decorator Factories
-function Logger2(logString) {
-    //Decorator
-    return function (constructor) {
-        console.log(logString);
-        console.log('constructor', constructor);
-    };
-}
-let Person2 = class Person2 {
-    constructor() {
-        this.name = 'Ronchi';
-        console.log('Creating Person Object....');
-    }
-};
-Person2 = __decorate([
-    Logger2('Logging decorator...')
-], Person2);
-const person2 = new Person2();
-console.log(person2);
-//3. Building more useful decorator
-// function Logger3(logString: string) {
+// const person = new Person();
+// console.log(person);
+// //2. Decorator Factories
+// function Logger2(logString: string) {
 //   //Decorator
 //   return function (constructor: Function) {
 //     console.log(logString);
 //     console.log('constructor', constructor);
 //   };
 // }
+// @Logger2('Logging decorator...')
+// class Person2 {
+//   name = 'Ronchi';
+//   constructor() {
+//     console.log('Creating Person Object....');
+//   }
+// }
+// const person2 = new Person2();
+// // console.log(person2);
+//3. Building more useful decorator
+//4. Adding multiple decorator
+function Logger3(logString) {
+    //Decorator
+    console.log('Logger Factory');
+    return function (constructor) {
+        console.log(logString);
+        console.log('constructor', constructor);
+    };
+}
 function WithTemplate(template, hookId) {
+    console.log('Template Factory');
     return function (constructor) {
         const element = document.getElementById(hookId);
+        console.log('Logging Template...');
         if (element) {
             const per = new constructor();
             element.innerHTML = template;
@@ -66,7 +66,8 @@ let Person3 = class Person3 {
     }
 };
 Person3 = __decorate([
+    Logger3('Logging 3....'),
     WithTemplate('<h1>Hello World!</h1>', 'app')
 ], Person3);
-const person3 = new Person2();
+const person3 = new Person3();
 console.log(person3);
--- app.ts
+++ app.ts
@@ -1,54 +1,59 @@
-//1. Decorators
-function Logger(constructor: Function) {
-  //Decorator
-  console.log('Logging...');
-  console.log('constructor', constructor);
-}
-@Logger
-class Person {
-  name = 'Ronchi';
-  constructor() {
-    console.log('Creating Person Object....');
-  }
-}
+// //1. Decorators
+// function Logger(constructor: Function) {
+//   //Decorator
+//   console.log('Logging...');
+//   console.log('constructor', constructor);
+// }
+// @Logger
+// class Person {
+//   name = 'Ronchi';
+//   constructor() {
+//     console.log('Creating Person Object....');
+//   }
+// }
 
-const person = new Person();
+// const person = new Person();
 
-console.log(person);
+// console.log(person);
 
-//2. Decorator Factories
-function Logger2(logString: string) {
-  //Decorator
-  return function (constructor: Function) {
-    console.log(logString);
-    console.log('constructor', constructor);
-  };
-}
-@Logger2('Logging decorator...')
-class Person2 {
-  name = 'Ronchi';
-  constructor() {
-    console.log('Creating Person Object....');
-  }
-}
-
-const person2 = new Person2();
-
-console.log(person2);
-
-//3. Building more useful decorator
-// function Logger3(logString: string) {
+// //2. Decorator Factories
+// function Logger2(logString: string) {
 //   //Decorator
 //   return function (constructor: Function) {
 //     console.log(logString);
 //     console.log('constructor', constructor);
 //   };
 // }
+// @Logger2('Logging decorator...')
+// class Person2 {
+//   name = 'Ronchi';
+//   constructor() {
+//     console.log('Creating Person Object....');
+//   }
+// }
+
+// const person2 = new Person2();
+
+// // console.log(person2);
+
+//3. Building more useful decorator
+//4. Adding multiple decorator
+function Logger3(logString: string) {
+  //Decorator
+  console.log('Logger Factory');
+  
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log('constructor', constructor);
+  };
+}
 
 function WithTemplate(template: string, hookId: string) {
+  console.log('Template Factory');
   return function (constructor: any) {
     const element = document.getElementById(hookId);
-
+    console.log('Logging Template...');
+    
     if (element) {
       const per = new constructor()
       element.innerHTML = template;
@@ -56,7 +61,7 @@ function WithTemplate(template: string, hookId: string) {
     }
   };
 }
-
+@Logger3('Logging 3....')
 @WithTemplate('<h1>Hello World!</h1>', 'app')
 class Person3 {
   name = 'Ronchi';
@@ -65,6 +70,6 @@ class Person3 {
   }
 }
 
-const person3 = new Person2();
+const person3 = new Person3();
 
 console.log(person3);
