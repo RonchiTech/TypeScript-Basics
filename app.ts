b41a2936b99091e4240abc2a870d004e598fb291
ronchitech <ronchibusiness@gmail.com>
2022-07-25 21:13:57 +0800

    interactive with DOM Elements

--- app.ts
+++ app.ts
@@ -2,7 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   formElement: HTMLFormElement;
-
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
   constructor() {
     this.templateElement = document.getElementById(
       'project-input'
@@ -13,8 +15,29 @@ class ProjectInput {
       true
     );
     this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    this.formElement.id = 'user-input';
+    this.titleInputElement = this.formElement.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.formElement.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.formElement.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+    this.configure();
     this.attach();
   }
+  private submitHandler(event: Event) {
+    console.log(event);
+    
+    event.preventDefault();
+    console.log('Logging...', this.titleInputElement.value);
+  }
+  private configure() {
+    this.formElement.addEventListener('submit', this.submitHandler.bind(this));
+  }
+
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.formElement);
   }
